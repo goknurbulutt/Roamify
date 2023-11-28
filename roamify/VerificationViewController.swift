66403638b76c321401c9279ba24b4fac2f0925af
goknurbulutt <120587181+goknurbulutt@users.noreply.github.com>
2023-11-28 23:51:57 +0300

    Firebase user creation settings were made and the user forgot my password screen was reset with firebase.

--- roamify/VerificationViewController.swift
+++ roamify/VerificationViewController.swift
@@ -6,36 +6,30 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class VerificationViewController: UIViewController {
-    
-    @IBOutlet weak var object: UIImageView!
-    
-    @IBOutlet weak var emailTextField: UITextField!
-    
-
-    
-  
-    
-    
-    
-    
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        
-            }
-            
-
-        
-       
-    
-    
-    @IBAction func sendLinkClicked(_ sender: Any) {
-    }
    
-    
-    
-    
+   @IBOutlet weak var object: UIImageView!
+   
+   @IBOutlet weak var emailTextField: UITextField!
+   
+   override func viewDidLoad() {
+       super.viewDidLoad()
+   }
+   
+   @IBAction func sendLinkClicked(_ sender: Any) {
+       guard let email = emailTextField.text, !email.isEmpty else {
+           return
+       }
+       
+       Auth.auth().sendPasswordReset(withEmail: email) { error in
+           if let error = error {
+               print("Şifre sıfırlama bağlantısı gönderilirken hata oluştu: \(error.localizedDescription)")
+           } else {
+               print("Şifre sıfırlama bağlantısı başarıyla gönderildi")
+           }
+       }
+   }
 }
+
