ad4fd11924e2d2882df94cb63459d610c799f1eb
goknurbulutt <120587181+goknurbulutt@users.noreply.github.com>
2023-11-29 01:18:57 +0300

    Empty data error edit.

--- roamify/VerificationViewController.swift
+++ roamify/VerificationViewController.swift
@@ -9,27 +9,50 @@ import UIKit
 import FirebaseAuth
 
 class VerificationViewController: UIViewController {
-   
-   @IBOutlet weak var object: UIImageView!
-   
-   @IBOutlet weak var emailTextField: UITextField!
-   
-   override func viewDidLoad() {
-       super.viewDidLoad()
-   }
-   
-   @IBAction func sendLinkClicked(_ sender: Any) {
-       guard let email = emailTextField.text, !email.isEmpty else {
-           return
-       }
-       
-       Auth.auth().sendPasswordReset(withEmail: email) { error in
-           if let error = error {
-               print("Şifre sıfırlama bağlantısı gönderilirken hata oluştu: \(error.localizedDescription)")
-           } else {
-               print("Şifre sıfırlama bağlantısı başarıyla gönderildi")
-           }
-       }
-   }
+
+    @IBOutlet weak var object: UIImageView!
+    @IBOutlet weak var emailTextField: UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func sendLinkClicked(_ sender: Any) {
+        guard let email = emailTextField.text, !email.isEmpty else {
+            errorMessage(titleInput: "Hata!", messageInput: "E-posta boş olmamalıdır!")
+            return
+        }
+
+        Auth.auth().sendPasswordReset(withEmail: email) { error in
+            if let error = error {
+                print("Error sending password reset link: \(error.localizedDescription)")
+            } else {
+                print("Password reset link sent successfully")
+                if self.emailTextField.text != ""{
+                    
+                    
+                    self.performSegue(withIdentifier: "toPRV", sender: nil)}
+            }
+            
+            
+        }
+    }
+    
+    
+    func errorMessage(titleInput: String, messageInput: String){
+        let alert = UIAlertController(title: titleInput, message: messageInput, preferredStyle: .alert
+        )
+        let okButton = UIAlertAction(title: "OK", style: .default , handler: nil)
+        alert.addAction(okButton)
+        
+        
+                                      
+        
+        
+    }
+    
 }
+
+
+
 
