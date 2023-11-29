116017c04bdaa68f53cf134616c85391229c39c1
Göknur Bulut <120587181+goknurbulutt@users.noreply.github.com>
2023-11-29 19:35:02 +0300

    Update VerificationViewController.swift

--- roamify/VerificationViewController.swift
+++ roamify/VerificationViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class VerificationViewController: UIViewController {
     
@@ -14,28 +15,46 @@ class VerificationViewController: UIViewController {
     @IBOutlet weak var emailTextField: UITextField!
     
 
-    
-  
-    
-    
-    
-    
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
-            }
+        }
             
 
-        
-       
-    
-    
-    @IBAction func sendLinkClicked(_ sender: Any) {
+        @IBAction func sendLinkClicked(_ sender: Any) {
+                                                   
+        guard let email = emailTextField.text, !email.isEmpty else {
+            errorMessage(titleInput: "Hata!", messageInput: "E-posta boş olmamalıdır!")
+            return
+        }
+
+        Auth.auth().sendPasswordReset(withEmail: email) { error in
+            if let error = error {
+                print("Error sending password reset link: \(error.localizedDescription)")
+            } else {
+                print("Password reset link sent successfully")
+                if self.emailTextField.text != ""{
+                    
+                    
+                    self.performSegue(withIdentifier: "toPRV", sender: nil)}
+            }
+            
+            
+        }
     }
-   
     
     
+    func errorMessage(titleInput: String, messageInput: String){
+        let alert = UIAlertController(title: titleInput, message: messageInput, preferredStyle: .alert
+        )
+        let okButton = UIAlertAction(title: "OK", style: .default , handler: nil)
+        alert.addAction(okButton)
+        
+        
+                                      
+        
+        
+    }
     
 }
+
+    
